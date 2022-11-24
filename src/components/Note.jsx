2cde1eb6fca9c427842e3fcb8bb1cab3eeb805e1
AnkitK <cosmicyokai100@gmail.com>
2022-11-24 18:56:04 +0530

    Initital commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>This is note title</h1>
+            <p>This is note content.</p>
+        </div>
+    );
+}
+
+export default Note;
